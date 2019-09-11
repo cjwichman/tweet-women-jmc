0556019a4ec61691fe88997fbe1109cf9742a0bd
Casey Wichman <caseywichman@gmail.com>
2019-09-11 17:56:05 -0500

    Updating script to create thread, formatted tweets, added quality control checks.

--- tweet.R
+++ tweet.R
@@ -34,24 +34,67 @@ setup_twitter_oauth(consumer_key    = "",
 ######################################################################################################
 candidates <- read_excel(paste0(dir,"/data/candidate_list.xlsx"))
 candidates$name.lc <- tolower(gsub(" ", "", candidates$name, fixed = TRUE))  # remove spaces, change to lowercase
+candidates$jmptitle.trim <- strtrim(candidates$jmptitle, 80) # limit length of title to 80 characters 
+
+
+
+# quality control notes
+# is JMC already posted to twitter? no=0, yes=1
+# generic quality control flag? good=0, bad=1
+## only post if flag == 0
+
+candidates$flag <- candidates$posted + candidates$qflag
 
 
 
 
-# generate text of tweet and URL for abstract image
-candidates$tweet.text <- paste0(candidates$name, ", ", candidates$university, ". JMP: ", candidates$jmptitle, ".", " Web: ", candidates$website)
+# generate text of tweet and filepath for abstract images
+candidates$tweet.text <- paste0(candidates$name, 
+                                ", ", 
+                                candidates$university, 
+                                ", ", 
+                                candidates$department,
+                                "\r\n \r\n", 
+                                "JMP: ", 
+                                candidates$jmptitle.trim,
+                                "\r\n \r\n", 
+                                "Web: ", 
+                                candidates$website)
 candidates$abstract <-  paste0(dir, "/abstractimages/", candidates$name.lc, ".png")
 
 
 
+
+
+######################################################################################################
+# Post thread to twitter
+######################################################################################################
+
+# initiate tweet for thread
+female.jmc.thread <- updateStatus(text = "Testing thread for JMCs #hashtag \r\n [your text here]")  
+
+
 # tweet JMC info!
-delay = 5 # in seconds
-for (i in 1:length(candidates)) {
-  tweet.jmc <- updateStatus(candidates$tweet.text[i], 
-                            mediaPath = candidates$abstract[i], 
-                            placeID = NULL, lat = NULL, long = NULL, bypassCharLimit = FALSE)
-  Sys.sleep(delay)
-}
+delay = 1 # in seconds
+for (i in 1:nrow(candidates)) {
+  
+  # set reply-to tweet to generate thread
+  if ( i==1 ) { replytotweet <- female.jmc.thread$id } 
+  else { replytotweet <- tweet.jmc$id }
+  
+  # should i post this tweet?
+  if ( candidates$flag[i] == 0 ) { # only tweet jmc if posted=0 and qflag==0 
+    
+    # post individual jmc tweets
+    tweet.jmc <- updateStatus(candidates$tweet.text[i], 
+                              mediaPath = candidates$abstract[i], 
+                              inReplyTo = replytotweet,
+                              placeID = NULL, lat = NULL, long = NULL, bypassCharLimit = TRUE)
+    Sys.sleep(delay)
+    
+  } ## closes quality control loop
+} ## closes tweet loop
+
 
 
 
